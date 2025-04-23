7a07501c1a64bda4158a2d87e50188aa72c991c5
LuisCarlosQuirosObando <lquiros460787@ufide.ac.cr>
2025-04-22 21:33:30 -0600

    Estructura y conexión BD 2
    Models and Views

---
+++ app/controllers/InventarioController.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../config/database.php';
+
+$sql = "SELECT materialId, nombre, descripcion, cantidad FROM material WHERE deleted_at IS NULL";
+$result = $conn->query($sql);
+
+$materiales = [];
+
+while ($row = $result->fetch_assoc()) {
+    $materiales[] = $row;
+}
+
+echo json_encode($materiales);
